00b78327d5da57e21b3476062e7c422de11444e6
Pavel Křivánek <pavel.krivanek@gmail.com>
2012-10-30 09:43:14 +0100

    add template for OSX

---
+++ template-mac/PhobosDemo.app/Contents/MacOS/start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+SELF_PATH=$(cd -P -- "$(dirname -- "$0")" &&  pwd -P) && SELF_PATH=$SELF_PATH/$(basename -- "$0")
+
+# resolve symlinks
+while [ -h $SELF_PATH ]; do
+    DIR=$(dirname -- "$SELF_PATH")
+    SYM=$(readlink $SELF_PATH)
+    SELF_PATH=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)/$(basename -- "$SYM")
+done
+
+CUR_DIR=$(dirname "$SELF_PATH")
+CUR_DIR=$(dirname "$CUR_DIR")
+
+"$CUR_DIR"/Frameworks/XUL.framework/Versions/16.0/xulrunner -app "$CUR_DIR"/Resources/application.ini 
